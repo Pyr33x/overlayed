c1c7288f952258a421d64c8aa264e3383489873d
Mehdi <woahmehdi@gmail.com>
2024-08-07 14:48:31 +0330

    increased border radius

--- apps/web/src/components/react/button.tsx
+++ apps/web/src/components/react/button.tsx
@@ -4,7 +4,7 @@ import { cn } from "../../utils";
 import * as React from "react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-xs sm:text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
